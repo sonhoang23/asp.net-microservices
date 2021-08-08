9e8bcc621edcb1e51e48bbcd8ec4ac15ee086412
hoangson23 <hoangson.hust@gmail.com>
2021-08-08 16:23:25 +0700

    update discount

---
+++ src/Services/Discount/Discount.API/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Discount.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<List<Coupon>> GetDiscounts();
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
